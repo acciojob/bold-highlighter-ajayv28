d5ac67074b847df3ca1b74eb8b99a03da5a50490
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 21:42:27 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
+    const boldWords = document.querySelectorAll('strong');
 
+    boldWords.forEach(word => {
+        word.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
+  const boldWords = document.querySelectorAll('strong');
 
+
+    boldWords.forEach(word => {
+        word.style.color = 'black';
+    });
     
 }
